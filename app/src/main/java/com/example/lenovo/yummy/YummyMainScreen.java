0da0979d09c42ef026f5a4d3ebe372c79835abe9
Sinnlos <sinnlos@github.com>
2018-11-06 10:29:16 +0100

    initial commit

---
+++ app/src/main/java/com/example/lenovo/yummy/YummyMainScreen.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.yummy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YummyMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yummy_main_screen);
+    }
+}
